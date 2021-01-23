0616f157f3caccec476912bd80be32ccfbbdbf57
Dan <dan.arias@auth0.com>
2021-01-23 16:37:08 -0500

    Update SCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router } from "react-router-dom";
 
 import { App } from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { Router } from "react-router-dom";
+import { MenuProvider } from "./context/menu-context";
 import { Auth0ProviderWithHistory } from "./components/auth/auth0-provider-with-history";
 
-import "./index.scss";
 import history from "./history";
-import { MenuProvider } from "./context/menu-context";
+
+import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -22,8 +22,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
